3e8a2df00d8e59ab9e52b3abeda6c16e9aa47fc3
WALWALASAFI <walwalasafi99@gmail.com>
2024-09-08 16:56:56 +0430

    work on app.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
+import React from 'react'; // This is for older versions of React
 import DrumMachine from './DrumMachine'; // Import DrumMachine component
 
-const App = () => {
-  return (
-    <div className="App">
-      <DrumMachine />
-    </div>
-  );
-};
+const App = () => (
+  <div className="App">
+    <DrumMachine />
+  </div>
+);
 
 export default App;
